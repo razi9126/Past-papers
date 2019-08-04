f3704e6a2ac7d6c452f0ad045ea083e2d211f31b
razi9126 <raziulhaq@Razi-ul-Haq.local>
2019-08-04 21:14:46 +0500

    First commit in branch

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const {MongoClient, ObjectId} = require('mongodb');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const PORT = process.env.PORT || 4001;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'pastpaper/build')));
+    
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'pastpaper/build', 'index.html'));
+  });
+}
+
+ 
+app.listen(PORT, () =>
+  console.log(`🚀 Server ready at http://localhost:${PORT}`)
+);
