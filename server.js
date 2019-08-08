10ca8b0306f8bddb067f09c506c1980ee919a6c6
razi9126 <raziulhaq@Razi-ul-Haq.local>
2019-08-09 00:43:31 +0500

    Added method to et permission. Added method which adds the file to a particular folder

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(bodyParser.json());
 
 // if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'pastpaper/build')));
-    
+	
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'pastpaper/build', 'index.html'));
+	res.sendFile(path.join(__dirname, 'pastpaper/build', 'index.html'));
   });
 // }
 
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Drive API.
-  authorize(JSON.parse(content), uploadFile);
+  authorize(JSON.parse(content), shareFolder);
 });
 
 /**
@@ -40,13 +40,13 @@ fs.readFile('credentials.json', (err, content) => {
 function authorize(credentials, callback) {
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+	  client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
+	if (err) return getAccessToken(oAuth2Client, callback);
+	oAuth2Client.setCredentials(JSON.parse(token));
+	callback(oAuth2Client);
   });
 }
 
@@ -58,26 +58,26 @@ function authorize(credentials, callback) {
  */
 function getAccessToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
+	access_type: 'offline',
+	scope: SCOPES,
   });
   console.log('Authorize this app by visiting this url:', authUrl);
   const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+	input: process.stdin,
+	output: process.stdout,
   });
   rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
+	rl.close();
+	oAuth2Client.getToken(code, (err, token) => {
+	  if (err) return console.error('Error retrieving access token', err);
+	  oAuth2Client.setCredentials(token);
+	  // Store the token to disk for later program executions
+	  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+		if (err) return console.error(err);
+		console.log('Token stored to', TOKEN_PATH);
+	  });
+	  callback(oAuth2Client);
+	});
   });
 }
 
@@ -88,44 +88,80 @@ function getAccessToken(oAuth2Client, callback) {
 function listFiles(auth) {
   const drive = google.drive({version: 'v3', auth});
   drive.files.list({
-    pageSize: 10,
-    fields: 'nextPageToken, files(id, name)',
+	pageSize: 10,
+	fields: 'nextPageToken, files(id, name)',
   }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const files = res.data.files;
-    if (files.length) {
-      console.log('Files:');
-      files.map((file) => {
-        console.log(`${file.name} (${file.id})`);
-      });
-    } else {
-      console.log('No files found.');
-    }
+	if (err) return console.log('The API returned an error: ' + err);
+	const files = res.data.files;
+	if (files.length) {
+	  console.log('Files:');
+	  files.map((file) => {
+		console.log(`${file.name} (${file.id})`);
+	  });
+	} else {
+	  console.log('No files found.');
+	}
   });
 }
 
 function uploadFile(auth) {
-  const drive = google.drive({version: 'v3', auth});
-  const fileMetadata = {
-    'name': 'photo.jpg'
-  };
-  const media = {
-    mimeType: 'image/jpeg',
-    body: fs.createReadStream('files/photo.jpeg')
-  };
-  drive.files.create({
-    resource: fileMetadata,
-    media: media,
-    fields: 'id'
-  }, (err, file) => {
-    if (err) {
-      // Handle error
-      console.error(err);
-    } else {
-      console.log(file);    	
-      console.log('File Id: ', file.data.id);
-    }
-  });
+	const drive = google.drive({version: 'v3', auth});
+	var folderId = '1GfTFUiZQmhpHLAYv-7AX0xQRsrOtgbFk';
+	var fileMetadata = {
+	  'name': 'photo.jpg',
+	  parents: [folderId]
+	};
+	var media = {
+	  mimeType: 'image/jpeg',
+	  body: fs.createReadStream('files/photo.jpeg')
+	};
+	drive.files.create({
+	  resource: fileMetadata,
+	  media: media,
+	  fields: 'id'
+	}, function (err, file) {
+	  if (err) {
+	    // Handle error
+	    console.error(err);
+	  } else {
+	    console.log('File Id: ', file.data.id);
+	  }
+	});
+}
+
+function shareFolder(auth) {
+	var permissions ={
+	    'type': 'anyone',
+	    'role': 'reader',
+	  };
+	const drive = google.drive({version: 'v3', auth});
+	drive.permissions.create({
+	      fileId: '1GfTFUiZQmhpHLAYv-7AX0xQRsrOtgbFk',
+	      resource: permissions,
+	  }, function(err,result){
+	        if(err) console.log(err) 
+	        else console.log(result)
+	    });
+}
+
+
+function makeFolder(auth) {
+	const drive = google.drive({version: 'v3', auth});
+	var fileMetadata = {
+	  'name': 'Question-Images',
+	  'mimeType': 'application/vnd.google-apps.folder'
+	};
+	drive.files.create({
+	  resource: fileMetadata,
+	  fields: 'id'
+	}, function (err, file) {
+	  if (err) {
+		// Handle error
+		console.error(err);
+	  } else {
+		console.log('Folder Id: ', file);
+	  }
+	});
 }
 
 app.post('/upload-question', (req, res) => {
