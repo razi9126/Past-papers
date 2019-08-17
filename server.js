aaeaf3488f345991ee592d74a106c361d8d75176
razi9126 <raziulhaq@Razi-ul-Haq.local>
2019-08-17 20:03:51 +0500

    Tags

--- server.js
+++ server.js
@@ -87,10 +87,24 @@ function getAccessToken(oAuth2Client, callback) {
   });
 }
 
+function mytags(){
+	let tagRef = db.collection('tags')
+
+  	let alltags = tagRef.get()
+  	.then(snapshot => {
+	    snapshot.forEach(doc => {
+	      console.log(doc.id, '=>', doc.data());
+	    });
+	  })
+	  .catch(err => {
+	    console.log('Error getting documents', err);
+	  });
+}
 
 function saveAuth(auth) {
 	auth_data = auth;
 	console.log("Authentication complete");
+	mytags()
 	// uploadFile(auth_data,'photo.jpeg');
 }
 
@@ -269,6 +283,55 @@ app.post('/find-questions', (req,res)=>{
 			console.log("DB error while finding questions: ", error)
 			res.send("error")
 		})
+
+})
+
+
+app.post('/add-tags', (req,res)=>{
+	let tagRef = db.collection('tags');
+	val_to_add = req.body.value
+	console.log(val_to_add)
+
+	let query = tagRef.where('value', '==', val_to_add).get()
+	  .then(snapshot => {
+	    if (snapshot.empty) {
+	      
+		    let data = {
+				value: (req.body.value)
+			}
+			let addTags = db.collection('tags').add(data)
+				.then(ref =>{
+					res.send("Tag Added")
+					console.log("Added tag with id: ", ref.id)
+				})
+				.catch(error=>{
+					res.send("Couldn't add tag to DB")
+					console.log("DB error while adding tag: ", error)
+				})
+		}
+	    else{
+	    	res.status(200).send("The tag already exists")
+	    }  
+	  })
+	  .catch(err => {
+	    console.log('Error getting documents', err);
+	  });
+})
+
+app.get('/find-tags', (req,res)=>{
+	// let ret = []
+	// let tagRef = db.collection('tags')
+
+ //  	let alltags = tagRef.get()
+ //  	.then(snapshot => {
+	//     snapshot.forEach(doc => {
+	//       console.log(doc.id, '=>', doc.data());
+	//     });
+	//   })
+	//   .catch(err => {
+	//     console.log('Error getting documents', err);
+	//   });
+	mytags()
 
 })
 
