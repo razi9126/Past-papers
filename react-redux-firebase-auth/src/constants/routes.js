87cbd9f22af76e8c8e259842dd10de17b3fe3a71
razi9126 <raziulhaq@Razi-ul-Haq.local>
2019-08-27 14:59:52 +0500

    committing

--- react-redux-firebase-auth/src/constants/routes.js
+++
@@ -1,6 +0,0 @@
-export const HOME = '/';
-export const SIGN_UP = '/signup';
-export const SIGN_IN = '/signin';
-export const PASSWORD_CHANGE = '/pw-change';
-export const PASSWORD_FORGET = '/pw-forget';
-export const ACCOUNT = '/account';
